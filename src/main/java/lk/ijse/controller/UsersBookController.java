6d6b9d2118e913a61af241e44401e5f47e90b613
UvinduV <uvindusanketh470@gmail.com>
2024-03-15 21:57:19 +0530

    create UsersBook Form

---
+++ src/main/java/lk/ijse/controller/UsersBookController.java
@@ -0,0 +1,75 @@
+package lk.ijse.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.bo.BoFactory;
+import lk.ijse.bo.custom.BookBo;
+import lk.ijse.dto.BookDto;
+
+import java.io.IOException;
+
+public class UsersBookController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private Label txtName;
+
+    @FXML
+    private TextField txtSearchBook;
+
+    BookBo bookBo = (BookBo) BoFactory.getBOFactory().getBo(BoFactory.BoTypes.BOOK);
+
+
+
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+        String title = txtSearchBook.getText();
+
+        try{
+            BookDto bookDto = bookBo.searchBook(title);
+
+            if(bookDto != null){
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/SearchBookForm.fxml"));
+                Parent rootNode = loader.load();
+
+                SearchBookFormController detailsController = loader.getController();
+
+                detailsController.searchBookDetails(bookDto);
+
+                Scene scene = new Scene(rootNode);
+
+                Stage stage = new Stage();
+                stage.setTitle("Book Worm");
+                stage.setScene(scene);
+                stage.show();
+
+                clearFields();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Book Not Found").show();
+            }
+        }catch(IOException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+
+
+    }
+
+
+    private void clearFields() {
+        txtSearchBook.setText("");
+    }
+}
